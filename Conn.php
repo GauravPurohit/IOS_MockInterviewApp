f717e51b66c9e23152b36670d8d134e0f57d50aa
GauravPurohit <gauravpurohit.info@gmail.com>
2016-05-04 21:48:15 -0500

    file changes

--- Conn.php
+++ Conn.php
@@ -9,9 +9,9 @@
 
 class Conn {
 public static $dbhost = "localhost";
-public static $dbuser = "root";
-public static $dbpass = "root";
-public static $dbname = "MockInterview";
+public static $dbuser = "my_userName_here";
+public static $dbpass = "my_pass_here";
+public static $dbname = "my_db_name_here";
 }
 
 ?>
